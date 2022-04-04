e17513ad029ed4d9af9d5a80d5c7ffadae6da365
jeong-hyun-woo <jhw504166@gmail.com>
2022-04-04 21:18:54 +0900

    javastudy

---
+++ src/baekjoon/q2588.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class q2588 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
+                String b = sc.next();
+        System.out.println(a*5);
+        System.out.println(a*8);
+        System.out.println(a*3);
+        System.out.println();
+    }
+}
