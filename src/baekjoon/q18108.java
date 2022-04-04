6a051b8ccba65cfd031c092be7cfd8ac70037270
jeong-hyun-woo <jhw504166@gmail.com>
2022-04-04 20:26:46 +0900

    javastudy

---
+++ src/baekjoon/q18108.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class q18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y;
+        y = sc.nextInt();
+        System.out.println(y-543);
+
+    }
+}
